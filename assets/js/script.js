25baf754b2e963af7a98902628ab1ff0b7da734e
ckyle121 <carolinekyle21@gmail.com>
2021-12-30 13:39:22 -0600

    add event listener to submit when user hits enter on the keyboard; also prevent Default to keep page from refreshing when user clicks submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
-var buttonEl = document.querySelector("#save-task");
+// variables to reference HTML
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+// create function to create tasks and add them to the to-do list 
+var createTaskHandler = function(event) {
+
+    // keeps page from reloading after running this function
+    event.preventDefault();
+    
+    // creates new list for html document and adds it to bottom of list
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-  }
+  };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
