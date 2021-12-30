6e854ecacdc025013f306e767ccbd07ce8d45da8
ckyle121 <carolinekyle21@gmail.com>
2021-12-30 14:06:50 -0600

    enter task names and task types and add them to the to do list using js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,24 @@ var createTaskHandler = function(event) {
     // keeps page from reloading after running this function
     event.preventDefault();
     
+    // collect what user inputs as task
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
     // creates new list for html document and adds it to bottom of list
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    //give taskInfoEl a class name
+    taskInfoEl.className = "task-info";
+
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire item to list 
     tasksToDoEl.appendChild(listItemEl);
   };
 
