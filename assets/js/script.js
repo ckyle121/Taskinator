14c31049e57fad6b40b8d0c3a2159920f4f61e69
ckyle121 <carolinekyle21@gmail.com>
2021-12-31 14:59:05 -0600

    optimize code

--- assets/js/script.js
+++ assets/js/script.js
@@ -252,51 +252,23 @@ var saveTasks = function(){
 // create function to load data from local storage
 var loadTasks = function(){
     // get task items from local storage
-    tasks = localStorage.getItem("tasks");
+    savedTasks = localStorage.getItem("tasks");
 
     // check if tasks are empty (no tasks are in local storage)
-    if (tasks === null){
+    if (!tasks){
         tasks = [];
         return false;
     }
 
     // convert tasks from string back into array of objects
-    tasks = JSON.parse(tasks);
-    
-    // print task items on the page 
-    for (var i = 0; i < tasks.length; i++){
-        tasks[i] = taskIdCounter
+    savedTasks = JSON.parse(savedTasks);
 
-        var listItemEl = document.createElement("li");
-        listItemEl.className = "task-item";
-        listItemEl.setAttribute("data-task-id", tasks[i].id);
-
-        var taskInfoEl = document.createElement("div");
-        taskInfoEl.className = "task-info"
-        taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
-        listItemEl.appendChild(taskInfoEl);
-
-        var taskActionsEl = createTaskActions(tasks[i].id);
-        listItemEl.appendChild(taskActionsEl);
-
-        if (tasks[i].status === "to-do"){
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
-            tasksToDoEl.appendChild(listItemEl);
-        }
-
-        else if (tasks[i].status === "in progress"){
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
-            tasksInProgressEl.appendChild(listItemEl);
-        }
-
-        else if (tasks[i].status === "complete"){
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
-            tasksCompletedEl.appendChild(listItemEl);
-        }
-
-        taskIdCounter++
+    // loop through savedTasks array 
+    for (var i = 0; i < saveTasks.length; i++){
+        //pass each task object into the createtaskEl function
+        createTaskEl(saveTasks[i]);
     }
-};
+};    
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 
